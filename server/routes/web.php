4cdb9463e7091373f1a5e63101cd35129a8a4123
huynq2007 <nqhuy.sgt@gmail.com>
2021-10-18 17:54:49 +0900

    08 handle file_get_contents

--- server/routes/web.php
+++ server/routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view("posts");
+    return view('posts');
 });
 
-Route::get('/post', function () {
-    return view("post");
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    if (!file_exists($path)) {
+        abort(404);
+    }
+    $post = file_get_contents($path);
+    return view('post', compact('post'));
 });
