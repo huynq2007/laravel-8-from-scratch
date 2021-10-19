1c762839efc286b022ab54b16609e24e4c589185
huynq2007 <nqhuy.sgt@gmail.com>
2021-10-19 10:34:52 +0900

    11 Use file system class to read directory

--- server/app/Models/Post.php
+++ server/app/Models/Post.php
@@ -4,8 +4,19 @@
 namespace App\Models;
 
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
+
 class Post
 {
+    public static function all()
+    {
+        $files = File::files(resource_path("posts"));
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+    }
+
     public static function find($slug)
     {
         if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
--- server/routes/web.php
+++ server/routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    $posts = Post::all();
+    return view('posts', compact('posts'));
 });
 
 Route::get('/posts/{post}', function ($slug) {
