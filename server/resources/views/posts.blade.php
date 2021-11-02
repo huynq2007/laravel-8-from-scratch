a1af4c619d6652a3942455b7c14674602495de69
huynq2007 <nqhuy.sgt@gmail.com>
2021-11-03 00:02:12 +0900

    23 Route Model Binding

--- server/resources/views/posts.blade.php
+++ server/resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @foreach($posts as $post)
         <h1>
-            <a href="/posts/{{ $post->id }}">
+            <a href="/posts/{{ $post->slug}}">
                 {{ $post->title }}
             </a>
         </h1>
